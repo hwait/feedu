6e60630caef46074b3f9ba0915a618dd7aa1c954
hwait <hwaitt@gmail.com>
2019-06-03 23:40:36 +0600

    Navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -6,146 +6,48 @@ import { actions as authActions } from '../../reducers/auth';
 import PropTypes from 'prop-types';
 class Navbar extends Component {
 	handleItemClick = (e, { name }) => {
-		this.props.pathChange(name);
+		const { pathChange, logout } = this.props;
+		name === 'logout' ? logout() : pathChange(name);
+	};
+	getItem = (name, title, path, icon) => {
+		return (
+			<Menu.Item
+				as={Link}
+				key={name}
+				name={name}
+				to={name === 'home' ? '/' : `/${name}`}
+				active={path === name}
+				onClick={this.handleItemClick}
+				className="menu-item"
+				color="blue"
+			>
+				{icon === '' ? (
+					<Image className="menu-avatar" src={this.props.user.avatar} circular centered />
+				) : (
+					<Icon name={icon} />
+				)}
+				{title}
+			</Menu.Item>
+		);
 	};
 
 	render() {
-		const { isAuthentificated, path, logout, user } = this.props;
+		const { isAuthentificated, path } = this.props;
 		console.log('====================================');
 		console.log(path);
 		console.log('====================================');
-		const linkHome = (
-			<Menu.Item
-				as={Link}
-				key="home"
-				name="home"
-				to="/"
-				active={path === 'home'}
-				onClick={this.handleItemClick}
-				className="menu-item"
-			>
-				<Icon name="home" />
-				Home
-			</Menu.Item>
-		);
-		const linkDashboard = (
-			<Menu.Item
-				className="menu-item"
-				as={Link}
-				key="dashboard"
-				name="home"
-				to="/"
-				active={path === 'home'}
-				onClick={this.handleItemClick}
-			>
-				<Icon name="lab" />
-				Board
-			</Menu.Item>
-		);
-		const linkCalendar = (
-			<Menu.Item
-				className="menu-item"
-				as={Link}
-				key="calendar"
-				name="calendar"
-				to="/calendar"
-				active={path === 'calendar'}
-				onClick={this.handleItemClick}
-			>
-				<Icon name="calendar" />
-				Calendar
-			</Menu.Item>
-		);
-		const linkPatterns = (
-			<Menu.Item
-				className="menu-item"
-				as={Link}
-				key="patterns"
-				name="patterns"
-				to="/patterns"
-				active={path === 'patterns'}
-				onClick={this.handleItemClick}
-			>
-				<Icon name="th" />
-				Pattern
-			</Menu.Item>
-		);
-		const linkLessons = (
-			<Menu.Item
-				as={Link}
-				key="lessons"
-				name="lessons"
-				to="/lessons"
-				active={path === 'lessons'}
-				onClick={this.handleItemClick}
-				className="menu-item"
-			>
-				<Icon name="flipboard" />
-				Lessons
-			</Menu.Item>
-		);
-		const linkBooks = (
-			<Menu.Item
-				as={Link}
-				key="books"
-				name="books"
-				to="/books"
-				active={path === 'books'}
-				onClick={this.handleItemClick}
-				className="menu-item"
-			>
-				<Icon name="book" />
-				Books
-			</Menu.Item>
-		);
-		const linkSignup = (
-			<Menu.Item
-				className="menu-item"
-				as={Link}
-				key="register"
-				name="register"
-				to="/register"
-				active={path === 'register'}
-				onClick={this.handleItemClick}
-			>
-				<Icon name="signup" />
-				Sign Up
-			</Menu.Item>
-		);
-		const linkProfile = (
-			<Menu.Item
-				className="menu-item"
-				as={Link}
-				key="profile"
-				name="profile"
-				to="/profile"
-				active={path === 'profile'}
-				onClick={this.handleItemClick}
-			>
-				<Image className="menu-avatar" src={user.avatar} circular centered />
-				Profile
-			</Menu.Item>
-		);
-		const linkCourse = (
-			<Menu.Item
-				className="menu-item"
-				as={Link}
-				key="course"
-				name="course"
-				to="/course"
-				active={path === 'course'}
-				onClick={this.handleItemClick}
-			>
-				<Icon name="paw" />
-				Course
-			</Menu.Item>
-		);
-		const linkLogout = (
-			<Menu.Item as={Link} key="logout" name="logout" to="/" onClick={logout} className="menu-item">
-				<Icon name="log out" />
-				Logout
-			</Menu.Item>
-		);
+		const linkHome = this.getItem('home', 'Home', path, 'home');
+		const linkDashboard = this.getItem('dashboard', 'Board', path, 'lab');
+		const linkCalendar = this.getItem('calendar', 'Calendar', path, 'calendar');
+		const linkPatterns = this.getItem('patterns', 'Pattern', path, 'th');
+
+		const linkLessons = this.getItem('lessons', 'Lesson', path, 'flipboard');
+		const linkBooks = this.getItem('books', 'Books', path, 'book');
+		const linkSignup = this.getItem('register', 'Sign Up', path, 'signup');
+		const linkProfile = this.getItem('profile', 'profile', path, '');
+		const linkCourse = this.getItem('course', 'Course', path, 'paw');
+		const linkLogout = this.getItem('logout', 'Logout', path, 'log out');
+
 		const menuItems = isAuthentificated
 			? [ linkDashboard, linkBooks, linkCourse, linkLessons, linkPatterns, linkCalendar, linkProfile, linkLogout ]
 			: [ linkHome, linkSignup ];
